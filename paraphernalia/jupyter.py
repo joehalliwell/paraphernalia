2a8bce980cc9a0c0438879fc3496adf590d4ccc9
Joe Halliwell <joe.halliwell@gmail.com>
2021-06-26 09:19:24 +0100

    Give audio scripts a play-by date

--- paraphernalia/jupyter.py
+++ paraphernalia/jupyter.py
@@ -2,40 +2,32 @@
 Tools for notebook-based work
 """
 import time
-from IPython.display import display, Audio, Javascript, HTML
+from IPython.display import display, Javascript
 
 # TODO: Assign credit for samples!
 DEFAULT_DING = "https://freesound.org/data/previews/80/80921_1022651-lq.ogg"
 COMPLETE_DING = "https://freesound.org/data/previews/122/122255_1074082-lq.mp3"
 
-# HACK: Wait for a bit for the message to arrive then clean up
-CLEANUP_DELAY = 0.3
+
+def run_once(js, timeout=10000):
+    expiry = time.time() * 1000 + timeout
+    widget = f"if (Date.now() <= {expiry}){{{js.strip()}}}"
+    display(Javascript(widget))
 
 
 def ding(url=DEFAULT_DING):
     url = url.replace("'", r"\'")
-    widget = f"""
-    (function() {{
-        var audio = new Audio('{url}');
-        audio.play();
-    }})();
-    """
-    handle = display(Javascript(widget), display_id=True)
-    time.sleep(CLEANUP_DELAY)
-    handle.update(Javascript(""))
+    run_once(f"new Audio('{url}').play();")
 
 
 def say(text):
     # Escape single quotes
     text = text.replace("'", r"\'")
-    widget = f"""
-    (function() {{
+    run_once(
+        f"""
         if (window.speechSynthesis) {{
             var synth = window.speechSynthesis;
             synth.speak(new window.SpeechSynthesisUtterance('{text}'));
         }}
-    }})();
-    """
-    handle = display(Javascript(widget), display_id=True)
-    time.sleep(CLEANUP_DELAY)
-    handle.update(Javascript(""))
+        """
+    )
