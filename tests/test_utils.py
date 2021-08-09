98a3a62d83de7138b32bb948c09f37d1b27a7c83
Joe Halliwell <joe.halliwell@gmail.com>
2021-08-09 08:36:21 +0100

    Add utils tests

---
+++ tests/test_utils.py
@@ -0,0 +1,20 @@
+from typing import Sized
+
+import pkg_resources
+from PIL import Image
+
+from paraphernalia.utils import *
+
+
+def test_cache_home():
+    cache = cache_home()
+    assert str(cache_home("FOO")) == "FOO"
+    cache_home(cache)
+
+
+def test_upscale():
+    studio = Image.open(pkg_resources.resource_filename(__name__, "studio.jpg"))
+    studio_x2 = opencv_to_pil(upsample(pil_to_opencv(studio), scale=2))
+    studio_x2.save("/tmp/test.jpg")
+    assert studio_x2.size[0] == 2 * studio.size[0]
+    assert studio_x2.size[1] == 2 * studio.size[1]
