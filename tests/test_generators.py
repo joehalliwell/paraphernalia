8ef3b5de4e6fd269e1125dce5f82eec8a4eca577
Joe Halliwell <joe.halliwell@gmail.com>
2021-08-14 20:27:47 +0100

    Consolidate generator tests

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,35 +1,22 @@
+import pytest
 from PIL.Image import Image
 
 from paraphernalia.torch.dall_e import DALL_E
 from paraphernalia.torch.direct import Direct, DirectPalette
 from paraphernalia.torch.siren import Siren
-from paraphernalia.torch.taming import VQGAN_GUMBEL_F8, Taming
+from paraphernalia.torch.taming import Taming
 
 
-def test_dall_e():
-    generator = DALL_E(latent=1)
-    img = generator.generate_image()
+@pytest.fixture(scope="module", params=[DALL_E, Direct, DirectPalette, Siren, Taming])
+def generator(request):
+    return request.param
+
+
+def test_init(generator):
+    img = generator().generate_image()
     assert isinstance(img, Image)
 
 
-def test_direct():
-    generator = Direct()
-    img = generator.generate_image()
+def test_sketch(generator, studio):
+    img = generator(start=studio).generate_image()
     assert isinstance(img, Image)
-
-
-def test_direct_palette():
-    generator = DirectPalette()
-    img = generator.generate_image()
-    assert isinstance(img, Image)
-
-
-def test_siren():
-    generator = Siren()
-    img = generator.generate_image()
-    assert isinstance(img, Image)
-
-
-def test_taming():
-    generator = Taming(model_spec=VQGAN_GUMBEL_F8)
-    generator.generate_image()
