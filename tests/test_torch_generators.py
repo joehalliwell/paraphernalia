bb45fd2b2b9c855028da70e25d2e04f9cfffb6f9
Joe Halliwell <joe.halliwell@gmail.com>
2021-10-05 06:42:23 +0100

    Rename test suite; add sizing test

--- tests/test_torch_generators.py
+++ tests/test_torch_generators.py
@@ -30,6 +30,13 @@ def test_init(generator):
     assert img.height == 32
 
 
+def test_sizing(generator):
+    img_s = generator(size=(64, 64)).generate_image()
+    img_m = generator(size=(128, 128)).generate_image()
+    assert img_s.width < img_m.width
+    assert img_s.height < img_m.height
+
+
 def test_sketch(generator, studio):
     img = generator(start=studio).generate_image()
     assert isinstance(img, Image)
